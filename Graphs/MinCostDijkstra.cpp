d2fe5d7773342584de057bbeb4a3453f8104e18c
Adilet Zhaxybay <adilet@adilet.org>
2014-05-26 01:06:32 +0600

    Added min-cost max-flow with Dijkstra (with potentials). Corrected Ford-Bellman version of min-cost max-flow.

--- Graphs/MinCostDijkstra.cpp
+++ Graphs/MinCostDijkstra.cpp
@@ -1,9 +1,9 @@
 /************************************************************************************
     
      Min Cost Flow (or Min Cost Max Flow) algorithm with
-     Ford-Bellman algorithm as shortest path search method. 
-     Works O(N ^ 6). Less on practice. 
-     Runs in O(N ^ 4) for bipartite matching case.
+     Dijkstra algorithm (with potentials) as shortest path search method. 
+     Works O(N ^ 5) or (N * M ^ 2 * logN). Less on practice. 
+     Runs in O(N ^ 3) for bipartite matching case.
      
      Based on problem 394 from informatics.mccme.ru 
      http://informatics.mccme.ru//mod/statements/view3.php?chapterid=394
@@ -41,37 +41,84 @@ struct edge {
 int n;
 int cost[MAXN][MAXN];
 vector <edge> e;
+vector <int> g[MAXN];
+long long phi[MAXN];
+priority_queue < pair < long long, int > > q;
 long long dist[MAXN];
 int par[MAXN];
 int edge_num;
 int s = 0, t = MAXN - 1;
+                      	
+void fordBellman() {
+	for (int i = s; i <= t; i++) 
+		dist[i] = INF;
+	dist[s] = 0;
+	while (true) {
+		bool change = false;
+		for (int i = 0; i < edge_num; i++) {
+			int from = e[i].from, to = e[i].to;
+			if (e[i].flow == e[i].cap)
+				continue;
+			if (dist[from] == INF)
+				continue;
+			if (dist[to] > dist[from] + e[i].cost) {
+				dist[to] = dist[from] + e[i].cost;
+				change = true;
+			}
+		}
+		if (!change)
+			break;
+	}
+}
+
+void dijkstra() {
+	while (!q.empty())
+		q.pop();
+	for (int i = s; i <= t; i++) {
+		dist[i] = INF;
+		q.push(make_pair(-dist[i], i));
+	}
+	dist[s] = 0;
+	q.push(make_pair(0, s));
+	while (!q.empty()) {
+		int cur = q.top().second;
+		long long cur_dist = -q.top().first;
+		q.pop();
+		if (cur_dist > dist[cur])
+			continue;
+		if (dist[cur] == INF)
+			break;
+		for (int i = 0; i < (int) g[cur].size(); i++) {
+			int ind = g[cur][i];
+			if (e[ind].flow == e[ind].cap)
+				continue;
+			int to = e[ind].to;
+			long long w = e[ind].cost + phi[cur] - phi[to];
+			if (cur_dist + w < dist[to]) {
+				dist[to] = cur_dist + w;
+				par[to] = ind;
+				q.push(make_pair(-dist[to], to));
+			}
+		}
+	}
+}
 
 long long minCost(int flow) {
 	long long result = 0;
-	while (true) {
-		for (int i = s; i <= t; i++)
-			dist[i] = INF;
-		dist[s] = 0;
-		while (true) {
-			bool change = false;
-			for (int i = 0; i < edge_num; i++) {
-				int from = e[i].from, to = e[i].to;
-				if (e[i].flow == e[i].cap)
-					continue;
-				if (dist[from] == INF)
-					continue;
-				if (dist[to] > dist[from] + e[i].cost) {
-					dist[to] = dist[from] + e[i].cost;
-					par[to] = i;
-					change = true;
-				}
-			}
-			if (!change)
-				break;
-		}
 
+	fordBellman();	
+	for (int i = s; i <= t; i++)
+		phi[i] = dist[i];
+
+	while (true) {
+	
+		dijkstra();
+		
 		if (dist[t] == INF)
 			return result;
+
+		for (int i = s; i <= t; i++)
+			phi[i] = min(phi[i] + dist[i], INF);
 
 		int push = flow;
 		int cur = t;
@@ -103,8 +150,12 @@ void addEdge(int from, int to, int cap, long long cost) {
 	edge tmp;
 	tmp.from = from; tmp.to = to; tmp.flow = 0; tmp.cap = cap; tmp.cost = cost;
 	e.push_back(tmp);
+	g[from].push_back(edge_num);
+
 	tmp.from = to; tmp.to = from; tmp.flow = cap; tmp.cap = cap; tmp.cost = -cost;
 	e.push_back(tmp);
+	g[to].push_back(edge_num + 1);
+
 	edge_num += 2;
 }
 
