90a09e36d35909d6c6c0f4e433a99c4049f4534a
Adilet Zhaxybay <adilet@adilet.org>
2014-05-30 20:23:33 +0600

    Added implicit segment tree. Modified old version of segment tree

--- DataStructures/SegmentTree(Assign-Sum).cpp
+++ DataStructures/SegmentTree(Assign-Sum).cpp
@@ -1,28 +1,47 @@
 /**********************************************************************
 
 	Segment Tree with assignment and sum on the interval
-	Can be used in problem D from CFR #200: 
-	http://codeforces.ru/contest/343/problem/D
 
-	TODO: find easier problem for this algorithm.
+	Based on problem C from http://codeforces.ru/gym/100093
 
 **********************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cassert>
+#include <utility>
+#include <iomanip>
+
+using namespace std;
 
 const int MAXN = 105000;
 const int zero = -1;
 
 struct node {
-	int sum;
+	long long sum;
 	int val;
 	int size;
 };
 
-int n, x, l, r;
+int n, m;
+char qt; 
+int x, l, r;
 int a[MAXN];
 vector <node> tree;
 
 void build (int v, int L, int R, int a[]) {    	
-	if ( L == R ) {
+	if (L == R) {
 		tree[v].sum = tree[v].val = a[L];
 		tree[v].size = 1;
 	}
@@ -37,22 +56,23 @@ void build (int v, int L, int R, int a[]) {
 }
 
 void push(int v) {
-	if (tree[v].val != zero) {
-		if (tree[v].size != 1) {
-			tree[2 * v].val = tree[v].val;
-			tree[2 * v + 1].val = tree[v].val;
-		}
-		tree[v].sum = tree[v].size * tree[v].val;
-		tree[v].val = zero;
+	if (tree[v].val == zero)
+		return;
+	if (tree[v].size != 1) {
+		tree[2 * v].val = tree[v].val;
+		tree[2 * v + 1].val = tree[v].val;
 	}
+	tree[v].sum = 1ll * tree[v].size * tree[v].val;
+	tree[v].val = zero;
 }
 
 void assign(int v, int L, int R, int l, int r, int val) {
-	if (l > r) return;
+	if (l > r) 
+		return;
 	push(v);
 	if (L == l && R == r) {
 		tree[v].val = val;
-		tree[v].sum = val * tree[v].size;
+		tree[v].sum = 1ll * val * tree[v].size;
 	} else {
 		int mid = L + (R - L) / 2;
 		assign(2 * v, L, mid, l, min(mid, r), val);
@@ -64,32 +84,37 @@ void assign(int v, int L, int R, int l, int r, int val) {
 	push(v);
 }
 
-int getsum(int v, int L, int R, int l, int r) {
-	if (l > r) return 0;
+long long getsum(int v, int L, int R, int l, int r) {
+	if (l > r) 
+		return 0;
 	push(v);
-	if (l == L && r == R) return tree[v].sum;
+	if (l == L && r == R) 
+		return tree[v].sum;
 	int mid = L + (R - L) / 2;
-	int res = getsum(2 * v, L, mid, l, min(mid, r)) + getsum(2 * v + 1, mid + 1, R, max(l, mid + 1), r);
+	long long res = getsum(2 * v, L, mid, l, min(mid, r)) + getsum(2 * v + 1, mid + 1, R, max(l, mid + 1), r);
 	return res;
 }
 
 int main() {
-	//Reading input
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++) 
-		scanf("%d", &a[i]);
+	assert(freopen("sum.in","r",stdin));
+	assert(freopen("sum.out","w",stdout));
 
-	//Setting up the segment tree
+	scanf("%d %d\n", &n, &m);
+
 	tree.resize(4 * n);
 	build(1, 1, n, a);
 
-	//Assigning the value x on l, r
-	scanf("%d %d %d", &l, &r, &x);
-	assign(1, 1, n, l, r, x);
-
-	//Getting the sum on l, r
-	scanf("%d %d", &l, &r);
-	cout << getsum(1, 1, n, l, r);
-
+	for (int i = 1; i <= m; i++) {
+		scanf("%c", &qt);
+		if (qt == 'A') {
+			scanf("%d %d %d\n", &l, &r, &x);
+			assign(1, 1, n, l, r, x);
+		}
+		else {
+			scanf("%d %d\n", &l, &r);
+			cout << getsum(1, 1, n, l, r) << endl;
+		}
+	}
+	
 	return 0;
 }
