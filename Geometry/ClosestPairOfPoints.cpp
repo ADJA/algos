19595014d54c1b4f0d5b3240450131c5cd0063c6
Adilet Zhaxybay <adilet@adilet.org>
2014-06-03 16:51:21 +0600

    Fixed code for closest pair of points

--- Geometry/ClosestPairOfPoints.cpp
+++ Geometry/ClosestPairOfPoints.cpp
@@ -1,94 +1,125 @@
-/***********
+/**********************************************************************************
 
-Solution to Closest Pair of Points Problem. O(nlogn) Divide-and-Conquer.
-Tested on http://www.spoj.com/problems/CLOPPAIR/
+	Finding the closest pair of points. O(NlogN), divide-and-conquer.
+	Based on http://www.spoj.com/problems/CLOPPAIR/
 
-***********/
+**********************************************************************************/
+
 #include <iostream>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
 #include <cstdlib>
 #include <cstdio>
-#include <algorithm>
-#include <cmath>
+#include <string>
+#include <cstring>
+#include <cassert>
+#include <utility>
+#include <iomanip>
 
 using namespace std;
 
-#define sqr(a) ((a)*(a))
-const double inf = 1e100;
-const int MAXN = 55000;
+#define sqr(x) ((x) * (x))
 
+const double inf = 1e100;   
+const int MAXN = 105000;
 
 struct point {
 	double x, y;
 	int ind;
-	void read() {
-		scanf("%lf%lf", &x, &y);
-	}
-	double dist_to(point& r) {
-		return sqrt(sqr(x - r.x) + sqr(y - r.y));
-	}
-	bool operator<(const point&r) const {
-		return x < r.x || (x == r.x && y < r.y);
-	}
 };
 
-point aux[MAXN], P[MAXN];
+bool cmp(point a, point b) {
+	return (a.x < b.x || (a.x == b.x && a.y < b.y));
+}
 
-int a, b, n;
+double dist(point a, point b) {
+	return sqrt(sqr(a.x - b.x) + sqr(a.y - b.y));
+}
+
+int n;
+int a[MAXN];
+point p[MAXN], tmp[MAXN];
 double ans = inf;
+int p1, p2;
 
-// ans contains closest distance, a, b - indices of points.
-double closest_pair(point p[], int n) {
-	if (n <= 1) return inf;
-	if (n == 2) {
-		if (p[0].y > p[1].y)
-			swap(p[0], p[1]);
-		double d = p[0].dist_to(p[1]);
-		if (d < ans)
-			ans = d, a = p[0].ind, b = p[1].ind;
-		return d;
+void updateAnswer(point a, point b) {
+	double d = dist(a, b);
+	if (d < ans) {
+		ans = d;
+		p1 = a.ind; p2 = b.ind;
 	}
-	int m = n / 2;
-	double dl = closest_pair(p, m); // left
-	double dr = closest_pair(p + m, n - m); //right
-	double x = p[m].x;
-	double d = min(dl, dr); 
-	int il = 0, ir = m, i = 0;
-	while (il < m && ir < n) { // merging two halves
-		if (p[il].y < p[ir].y)
-			aux[i ++] = p[il ++];
-		else
-			aux[i ++] = p[ir ++];
-	}
-	while (il < m)
-		aux[i ++] = p[il ++];
-	while (ir < n)
-		aux[i ++] = p[ir ++];
-	for (int j = 0 ; j < n ; j ++)
-		p[j] = aux[j];
-	for (int j = 0 ; j < n ; j ++) { // 2d x d box
-		for (int k = j + 1 ; k < n && p[j].dist_to(p[k]) < d ; k ++) {
-			if (d > p[j].dist_to(p[k])) {
-				d = p[j].dist_to(p[k]);
-				if (ans > d) {
-					ans = d;
-					a = p[k].ind, b = p[j].ind;
-				}
-			}			
+}
 
+void closestPair(int l, int r) {
+	if (l >= r)
+		return;
+
+	if (r - l == 1) {
+		if (p[l].y > p[r].y)
+			swap(p[l], p[r]);
+		updateAnswer(p[l], p[r]);
+		return;
+	}
+
+	int m = (l + r) / 2;
+	double mx = p[m].x;
+
+	closestPair(l, m);
+	closestPair(m + 1, r);
+
+	int lp = l, rp = m + 1, sz = 1;
+	while (lp <= m || rp <= r) {
+		if (lp > m || ((rp <= r && p[rp].y < p[lp].y))) {
+			tmp[sz] = p[rp];
+			rp++;
+		}
+		else {
+			tmp[sz] = p[lp];
+			lp++;
+		}
+		sz++;
+	}
+
+	for (int i = l; i <= r; i++) 
+		p[i] = tmp[i - l + 1];
+
+	sz = 0;
+	for (int i = l; i <= r; i++)
+		if (abs(p[i].x - mx) < ans) {
+			sz++;
+			tmp[sz] = p[i];
+		}
+
+	for (int i = 1; i <= sz; i++) {
+		for (int j = i - 1; j >= 1; j--) {
+			if (tmp[i].y - tmp[j].y >= ans)
+				break;
+			updateAnswer(tmp[i], tmp[j]);
 		}
 	}
-	return d;
 }
 
 int main() {
+	//assert(freopen("input.txt","r",stdin));
+	//assert(freopen("output.txt","w",stdout));
+
 	scanf("%d", &n);
-	for (int i = 0 ; i < n ; i++) {
-		P[i].read();
-		P[i].ind = i;
+	for (int i = 1; i <= n; i++) {
+		scanf("%lf %lf", &p[i].x, &p[i].y);
+		p[i].ind = i - 1;
 	}
-	sort(P, P + n);
-	closest_pair(P, n);
-	printf("%d %d %lf\n", min(a, b), max(a, b), ans);
+
+	sort(p + 1, p + n + 1, cmp);
+
+	closestPair(1, n);
+
+	printf("%d %d %.6lf\n", min(p1, p2), max(p1, p2), ans);
+
 	return 0;
 }
-
